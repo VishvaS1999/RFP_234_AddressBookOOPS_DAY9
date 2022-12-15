1a1c528120fdd45b2665b45940b3dc86a73daf12
VishvaS1999 <gvishva2601@gmail.com>
2022-12-15 21:26:14 +0530

    Welcome Address Book System

---
+++ src/com/addressbook/WelcomeAddressBookSystem.java
@@ -0,0 +1,9 @@
+package com.addressbook;
+
+public class WelcomeAddressBookSystem
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome Address Book System!..");
+    }
+}
