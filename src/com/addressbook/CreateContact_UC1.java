7521eb78a87c2e0eebed05fcc72386eb175c986b
VishvaS1999 <gvishva2601@gmail.com>
2022-12-15 21:28:05 +0530

    Create Contact

---
+++ src/com/addressbook/CreateContact_UC1.java
@@ -0,0 +1,13 @@
+package com.addressbook;
+
+import java.util.Scanner;
+
+public class CreateContact_UC1
+{
+
+    public static void main(String[] args)
+    {
+        Contacts obj = new Contacts("vishva", "Santoki", "Dahisar", "Mumbai", "Maharashtra", "400068", "7359294419", "ghetiyavishva2601@gamil.com");
+        System.out.println(obj);
+    }
+}
